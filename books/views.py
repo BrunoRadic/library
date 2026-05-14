c84c9bb8ee522469f1b31e3589af3dbeb525fe3c
BrunoRadic <radicbruno0@gmail.com>
2026-05-14 16:23:51 +0200

    feat: book list view with title, author and genre filters

--- books/views.py
+++ books/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book, Genre
+
+
+@login_required
+def book_list(request):
+    books = Book.objects.select_related('author').prefetch_related('genres').all()
+
+    title = request.GET.get('title', '').strip()
+    author = request.GET.get('author', '').strip()
+    genre = request.GET.get('genre', '').strip()
+
+    if title:
+        books = books.filter(title__icontains=title)
+    if author:
+        books = books.filter(author__last_name__icontains=author)
+    if genre:
+        books = books.filter(genres__id=genre)
+
+    return render(request, 'books/book_list.html', {
+        'books': books,
+        'authors': Author.objects.all(),
+        'genres': Genre.objects.all(),
+    })
